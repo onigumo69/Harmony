9228ece4c47c226f9d74a3656069694525c6aa4e
Onigumo <onigumo69@outlook.com>
2021-10-15 12:53:42 +0800

    Clean up

--- Harmony/src/Harmony/Core/Log.cpp
+++ Harmony/src/Harmony/Core/Log.cpp
@@ -13,12 +13,12 @@ namespace Harmony
 	{
 		std::vector<spdlog::sink_ptr> logSinks;
 		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hazel.log", true));
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Harmony.log", true));
 
 		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
 		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
-		_core_logger = std::make_shared<spdlog::logger>("HAZEL", begin(logSinks), end(logSinks));
+		_core_logger = std::make_shared<spdlog::logger>("HARMONY", begin(logSinks), end(logSinks));
 		spdlog::register_logger(_core_logger);
 		_core_logger->set_level(spdlog::level::trace);
 		_core_logger->flush_on(spdlog::level::trace);
